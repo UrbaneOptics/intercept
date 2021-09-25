dc60d3e534da6f9de5236f665a811be3dd393b56
Joe James Rodriguez <joerodrig3@gmail.com>
2021-09-25 17:33:49 -0400

    Import 11/20 and 12/20

--- projects/nyc-moving-violation-data/scripts/excel_to_csv.sh
+++ projects/nyc-moving-violation-data/scripts/excel_to_csv.sh
@@ -1,5 +1,6 @@
 # This will convert all excel files in the current working directory to CSV files
 # and it will delete the excel files afterward.
+# Requires gnumeric to be installed(brew install gnumeric)
 find . -maxdepth 1 -name "*.xlsx" -exec ssconvert {} --export-type=Gnumeric_stf:stf_csv \;
 
 rm *.xlsx
