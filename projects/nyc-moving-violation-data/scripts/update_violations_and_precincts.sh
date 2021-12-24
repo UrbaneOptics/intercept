194ecd4717e9ed25988f07209048989060c2e219
Joe James Rodriguez <joerodrig3@gmail.com>
2021-12-23 22:13:15 -0500

    Update aggregation script to be run from root directory

--- projects/nyc-moving-violation-data/scripts/update_violations_and_precincts.sh
+++ projects/nyc-moving-violation-data/scripts/update_violations_and_precincts.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Run from CSV directory
-
-# Aggregate all csv's
-for file in **/*.csv; do cat "$file" >> merged.csv; done    
-
-# Collect names and output to file
-ruby ../../scripts/retrieve_violation_names.rb merged.csv
-
-rm merged.csv
+cd ./data/csv/ && \
+echo "Aggregating data" && \
+for file in ./**/*.csv; do cat "$file" >> merged.csv; done && \
+echo "Processing aggregates" && \
+ruby ../../scripts/retrieve_violation_names.rb merged.csv && \
+echo "Cleaning up artifacts" && \
+rm merged.csv && \
+echo "Aggregation complete. Validate and commit changes."
